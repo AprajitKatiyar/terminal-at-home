9d3c9c0035ead99d2f636a8a6e19d74b092a4c0f
Aprajit Katiyar <aprajit.katiyar1999@gmail.com>
2024-06-27 21:48:43 +0530

    Logic for client side handlers

--- frontend/src/components/Terminal.tsx
+++ frontend/src/components/Terminal.tsx
@@ -1,39 +1,64 @@
-import { Terminal } from "@xterm/xterm";
+import { Terminal } from "xterm";
 import React, { useEffect, useRef, useState } from "react";
+import { Socket } from "socket.io-client";
 import { io } from "socket.io-client";
+import { FitAddon } from "xterm-addon-fit";
 import "@xterm/xterm/css/xterm.css";
-
-const term = new Terminal();
+const fitAddOn = new FitAddon();
+const OPTIONS_TERM = {
+  useStyle: true,
+  screenKeys: true,
+  cursorBlink: true,
+  cols: 200,
+  theme: {
+    background: "black",
+  },
+};
+function ab2str(buf: ArrayBuffer): string {
+  const uint8Array = new Uint8Array(buf);
+  const charArray = Array.from(uint8Array);
+  return String.fromCharCode(...charArray);
+}
 
 const XTerminal: React.FC = () => {
   const terminalRef = useRef<HTMLDivElement>(null);
-  const [socket, setSocket] = useState<any | null>(null);
+  const [socket, setSocket] = useState<Socket | null>(null);
   useEffect(() => {
     const socket = io("http://localhost:3000");
     setSocket(socket);
-    // socket.emit("joinProject", project.projectId);
   }, []);
 
   useEffect(() => {
-    if (!terminalRef || !terminalRef.current) return;
-    term.open(terminalRef.current);
-    term.write("Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ");
-    term.onKey((e) => {
-      socket.emit("message", e.key);
-    });
-  }, [terminalRef]);
-  useEffect(() => {
-    if (socket == null) return;
-    const handleOutput = (data: string) => {
-      term.write(data);
-    };
+    if (!terminalRef || !terminalRef.current || !socket) return;
 
-    socket && socket?.on("output", handleOutput);
+    socket.emit("requestTerminal");
+    socket.on("terminal", terminalHandler);
+    const term = new Terminal(OPTIONS_TERM);
+    term.loadAddon(fitAddOn);
+    term.open(terminalRef.current);
+    fitAddOn.fit();
+    function terminalHandler({ data }: { data: ArrayBuffer }) {
+      if (data instanceof ArrayBuffer) {
+        console.error(data);
+        console.log(ab2str(data));
+        term.write(ab2str(data));
+      }
+    }
+    term.onData((data) => {
+      console.log(data);
+      socket.emit("terminalData", {
+        data,
+      });
+    });
+
+    socket.emit("terminalData", {
+      data: "\n",
+    });
 
     return () => {
-      socket && socket.off("output", handleOutput);
+      socket.off("terminal");
     };
-  }, [socket]);
+  }, [terminalRef, socket]);
 
   return <div ref={terminalRef} />;
 };
